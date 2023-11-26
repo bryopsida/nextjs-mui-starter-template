8cbaea1f5d7c91a1355580de49583a3358a645fe
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-11-26 17:14:26 -0600

    add material ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Container from '@mui/material/Container'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </head>
+      <body className={inter.className}>
+        <Container>{children}</Container>
+      </body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,16 @@
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import Typography from '@mui/material/Typography'
 import styles from './page.module.css'
 
 export default function Home() {
-  return <main className={styles.main}></main>
+  return (
+    <main className={styles.main}>
+      <Box>
+        <Card>
+          <Typography variant="h2">Hello World ~</Typography>
+        </Card>
+      </Box>
+    </main>
+  )
 }
