435e0ef7f217240cc7631e03946a3d2916b2f87d
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-12-03 07:43:16 -0600

    reset and use typescript

--- tests/login.spec.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-const { test, expect } = require('@playwright/test')
-
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/login')
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Login/)
-})
